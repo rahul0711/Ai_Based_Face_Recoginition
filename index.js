a661b576d0c98340b91bb9ef166629aedb9ef523
Sonu Santhosh <sonusanthosh972@gmail.com>
2026-02-16 17:15:32 +0530

    some new

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const app = express()
 
 app.use(
   cors({
-    origin: "*",
+    origin: "http://localhost:5173",
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
